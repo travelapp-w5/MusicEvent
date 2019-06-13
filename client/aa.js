757848456cef714e682fe450a71b898644f2f39b
Max Tedjojuwono <maxpt750@gmail.com>
2019-06-14 00:26:43 +0700

    new json

---
+++ client/aa.js
@@ -0,0 +1,963 @@
+let arr = [
+  {
+    "FIELD1": "Afghanistan",
+    "FIELD2": "Afghan afghani",
+    "FIELD3": "AFN"
+  },
+  {
+    "FIELD1": "Albania",
+    "FIELD2": "Albanian lek",
+    "FIELD3": "ALL"
+  },
+  {
+    "FIELD1": "Algeria",
+    "FIELD2": "Algerian dinar",
+    "FIELD3": "DZD"
+  },
+  {
+    "FIELD1": "Andorra",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Angola",
+    "FIELD2": "Angolan kwanza",
+    "FIELD3": "AOA"
+  },
+  {
+    "FIELD1": "Antigua and Barbuda",
+    "FIELD2": "East Caribbean dollar",
+    "FIELD3": "XCD"
+  },
+  {
+    "FIELD1": "Argentina",
+    "FIELD2": "Argentine peso",
+    "FIELD3": "ARS"
+  },
+  {
+    "FIELD1": "Armenia",
+    "FIELD2": "Armenian dram",
+    "FIELD3": "AMD"
+  },
+  {
+    "FIELD1": "Australia",
+    "FIELD2": "Australian dollar",
+    "FIELD3": "AUD"
+  },
+  {
+    "FIELD1": "Austria",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Azerbaijan",
+    "FIELD2": "Azerbaijani manat",
+    "FIELD3": "AZN"
+  },
+  {
+    "FIELD1": "Bahamas",
+    "FIELD2": "Bahamian dollar",
+    "FIELD3": "BSD"
+  },
+  {
+    "FIELD1": "Bahrain",
+    "FIELD2": "Bahraini dinar",
+    "FIELD3": "BHD"
+  },
+  {
+    "FIELD1": "Bangladesh",
+    "FIELD2": "Bangladeshi taka",
+    "FIELD3": "BDT"
+  },
+  {
+    "FIELD1": "Barbados",
+    "FIELD2": "Barbadian dollar",
+    "FIELD3": "BBD"
+  },
+  {
+    "FIELD1": "Belarus",
+    "FIELD2": "Belarusian ruble",
+    "FIELD3": "BYN"
+  },
+  {
+    "FIELD1": "Belgium",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Belize",
+    "FIELD2": "Belize dollar",
+    "FIELD3": "BZD"
+  },
+  {
+    "FIELD1": "Benin",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Bhutan",
+    "FIELD2": "Bhutanese ngultrum",
+    "FIELD3": "BTN"
+  },
+  {
+    "FIELD1": "Bolivia",
+    "FIELD2": "Bolivian boliviano",
+    "FIELD3": "BOB"
+  },
+  {
+    "FIELD1": "Bosnia and Herzegovina",
+    "FIELD2": "Bosnia and Herzegovina convertible mark",
+    "FIELD3": "BAM"
+  },
+  {
+    "FIELD1": "Botswana",
+    "FIELD2": "Botswana pula",
+    "FIELD3": "BWP"
+  },
+  {
+    "FIELD1": "Brazil",
+    "FIELD2": "Brazilian real",
+    "FIELD3": "BRL"
+  },
+  {
+    "FIELD1": "Brunei",
+    "FIELD2": "Brunei dollar",
+    "FIELD3": "BND"
+  },
+  {
+    "FIELD1": "Bulgaria",
+    "FIELD2": "Bulgarian lev",
+    "FIELD3": "BGN"
+  },
+  {
+    "FIELD1": "Burkina Faso",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Burundi",
+    "FIELD2": "Burundian franc",
+    "FIELD3": "BIF"
+  },
+  {
+    "FIELD1": "Cambodia",
+    "FIELD2": "Cambodian riel",
+    "FIELD3": "KHR"
+  },
+  {
+    "FIELD1": "Canada",
+    "FIELD2": "Canadian dollar",
+    "FIELD3": "CAD"
+  },
+  {
+    "FIELD1": "Cape Verde",
+    "FIELD2": "Cape Verdean escudo",
+    "FIELD3": "CVE"
+  },
+  {
+    "FIELD1": "Chad",
+    "FIELD2": "Central African CFA franc",
+    "FIELD3": "XAF"
+  },
+  {
+    "FIELD1": "Chile",
+    "FIELD2": "Chilean peso",
+    "FIELD3": "CLP"
+  },
+  {
+    "FIELD1": "China",
+    "FIELD2": "Chinese yuan",
+    "FIELD3": "CNY"
+  },
+  {
+    "FIELD1": "Colombia",
+    "FIELD2": "Colombian peso",
+    "FIELD3": "COP"
+  },
+  {
+    "FIELD1": "Comoros",
+    "FIELD2": "Comorian franc",
+    "FIELD3": "KMF"
+  },
+  {
+    "FIELD1": "Costa Rica",
+    "FIELD2": "Costa Rican col�n",
+    "FIELD3": "CRC"
+  },
+  {
+    "FIELD1": "Croatia",
+    "FIELD2": "Croatian kuna",
+    "FIELD3": "HRK"
+  },
+  {
+    "FIELD1": "Cuba",
+    "FIELD2": "Cuban peso",
+    "FIELD3": "CUP"
+  },
+  {
+    "FIELD1": "Cyprus",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Czech Republic",
+    "FIELD2": "Czech koruna",
+    "FIELD3": "CZK"
+  },
+  {
+    "FIELD1": "Democratic Republic of Congo",
+    "FIELD2": "Congolese franc",
+    "FIELD3": "CDF"
+  },
+  {
+    "FIELD1": "Denmark",
+    "FIELD2": "Danish krone",
+    "FIELD3": "DKK"
+  },
+  {
+    "FIELD1": "Djibouti",
+    "FIELD2": "Djiboutian franc",
+    "FIELD3": "DJF"
+  },
+  {
+    "FIELD1": "Dominica",
+    "FIELD2": "East Caribbean dollar",
+    "FIELD3": "XCD"
+  },
+  {
+    "FIELD1": "Dominican Republic",
+    "FIELD2": "Dominican peso",
+    "FIELD3": "DOP"
+  },
+  {
+    "FIELD1": "East Timor",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Ecuador",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Egypt",
+    "FIELD2": "Egyptian pound",
+    "FIELD3": "EGP"
+  },
+  {
+    "FIELD1": "El Salvador",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Equatorial Guinea",
+    "FIELD2": "Central African CFA franc",
+    "FIELD3": "XAF"
+  },
+  {
+    "FIELD1": "Eritrea",
+    "FIELD2": "Eritrean nakfa",
+    "FIELD3": "ERN"
+  },
+  {
+    "FIELD1": "Estonia",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Ethiopia",
+    "FIELD2": "Ethiopian birr",
+    "FIELD3": "ETB"
+  },
+  {
+    "FIELD1": "Fiji",
+    "FIELD2": "Fijian dollar",
+    "FIELD3": "FJD"
+  },
+  {
+    "FIELD1": "Finland",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "France",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Gambia",
+    "FIELD2": "Gambian dalasi",
+    "FIELD3": "GMD"
+  },
+  {
+    "FIELD1": "Georgia",
+    "FIELD2": "Georgian lari",
+    "FIELD3": "GEL"
+  },
+  {
+    "FIELD1": "Germany",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Ghana",
+    "FIELD2": "Ghanaian cedi",
+    "FIELD3": "GHS"
+  },
+  {
+    "FIELD1": "Greece",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Grenada",
+    "FIELD2": "East Caribbean dollar",
+    "FIELD3": "XCD"
+  },
+  {
+    "FIELD1": "Guatemala",
+    "FIELD2": "Guatemalan quetzal",
+    "FIELD3": "GTQ"
+  },
+  {
+    "FIELD1": "Guinea",
+    "FIELD2": "Guinean franc",
+    "FIELD3": "GNF"
+  },
+  {
+    "FIELD1": "Guinea-Bissau",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Guyana",
+    "FIELD2": "Guyanese dollar",
+    "FIELD3": "GYD"
+  },
+  {
+    "FIELD1": "Haiti",
+    "FIELD2": "Haitian gourde",
+    "FIELD3": "HTG"
+  },
+  {
+    "FIELD1": "Honduras",
+    "FIELD2": "Honduran lempira",
+    "FIELD3": "HNL"
+  },
+  {
+    "FIELD1": "Hungary",
+    "FIELD2": "Hungarian forint",
+    "FIELD3": "HUF"
+  },
+  {
+    "FIELD1": "Iceland",
+    "FIELD2": "Icelandic kr�na",
+    "FIELD3": "ISK"
+  },
+  {
+    "FIELD1": "India",
+    "FIELD2": "Indian rupee",
+    "FIELD3": "INR"
+  },
+  {
+    "FIELD1": "Indonesia",
+    "FIELD2": "Indonesian rupiah",
+    "FIELD3": "IDR"
+  },
+  {
+    "FIELD1": "Iran",
+    "FIELD2": "Iranian rial",
+    "FIELD3": "IRR"
+  },
+  {
+    "FIELD1": "Iraq",
+    "FIELD2": "Iraqi dinar",
+    "FIELD3": "IQD"
+  },
+  {
+    "FIELD1": "Ireland",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Israel",
+    "FIELD2": "Israeli new shekel",
+    "FIELD3": "ILS"
+  },
+  {
+    "FIELD1": "Italy",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Ivory Coast",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Jamaica",
+    "FIELD2": "Jamaican dollar",
+    "FIELD3": "JMD"
+  },
+  {
+    "FIELD1": "Japan",
+    "FIELD2": "Japanese yen",
+    "FIELD3": "JPY"
+  },
+  {
+    "FIELD1": "Jordan",
+    "FIELD2": "Jordanian dinar",
+    "FIELD3": "JOD"
+  },
+  {
+    "FIELD1": "Kazakhstan",
+    "FIELD2": "Kazakhstani tenge",
+    "FIELD3": "KZT"
+  },
+  {
+    "FIELD1": "Kenya",
+    "FIELD2": "Kenyan shilling",
+    "FIELD3": "KES"
+  },
+  {
+    "FIELD1": "Kiribati",
+    "FIELD2": "Australian dollar",
+    "FIELD3": "AUD"
+  },
+  {
+    "FIELD1": "Korea, North",
+    "FIELD2": "North Korean won",
+    "FIELD3": "KPW"
+  },
+  {
+    "FIELD1": "Korea, South",
+    "FIELD2": "South Korean won",
+    "FIELD3": "KRW"
+  },
+  {
+    "FIELD1": "Kosovo",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Kuwait",
+    "FIELD2": "Kuwaiti dinar",
+    "FIELD3": "KWD"
+  },
+  {
+    "FIELD1": "Kyrgyzstan",
+    "FIELD2": "Kyrgyzstani som",
+    "FIELD3": "KGS"
+  },
+  {
+    "FIELD1": "Laos",
+    "FIELD2": "Lao kip",
+    "FIELD3": "LAK"
+  },
+  {
+    "FIELD1": "Latvia",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Lebanon",
+    "FIELD2": "Lebanese pound",
+    "FIELD3": "LBP"
+  },
+  {
+    "FIELD1": "Lesotho",
+    "FIELD2": "Lesotho loti",
+    "FIELD3": "LSL"
+  },
+  {
+    "FIELD1": "Liberia",
+    "FIELD2": "Liberian dollar",
+    "FIELD3": "LRD"
+  },
+  {
+    "FIELD1": "Libya",
+    "FIELD2": "Libyan dinar",
+    "FIELD3": "LYD"
+  },
+  {
+    "FIELD1": "Liechtenstein",
+    "FIELD2": "Swiss franc",
+    "FIELD3": "CHF"
+  },
+  {
+    "FIELD1": "Lithuania",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Luxembourg",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Macedonia",
+    "FIELD2": "Macedonian denar",
+    "FIELD3": "MKD"
+  },
+  {
+    "FIELD1": "Madagascar",
+    "FIELD2": "Malagasy ariary",
+    "FIELD3": "MGA"
+  },
+  {
+    "FIELD1": "Malawi",
+    "FIELD2": "Malawian kwacha",
+    "FIELD3": "MWK"
+  },
+  {
+    "FIELD1": "Malaysia",
+    "FIELD2": "Malaysian ringgit",
+    "FIELD3": "MYR"
+  },
+  {
+    "FIELD1": "Maldives",
+    "FIELD2": "Maldivian rufiyaa",
+    "FIELD3": "MVR"
+  },
+  {
+    "FIELD1": "Mali",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Malta",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Marshall Islands",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Mauritania",
+    "FIELD2": "Mauritanian ouguiya",
+    "FIELD3": "MRO"
+  },
+  {
+    "FIELD1": "Mauritius",
+    "FIELD2": "Mauritian rupee",
+    "FIELD3": "MUR"
+  },
+  {
+    "FIELD1": "Mexico",
+    "FIELD2": "Mexican peso",
+    "FIELD3": "MXN"
+  },
+  {
+    "FIELD1": "Micronesia",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Moldova",
+    "FIELD2": "Moldovan leu",
+    "FIELD3": "MDL"
+  },
+  {
+    "FIELD1": "Monaco",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Mongolia",
+    "FIELD2": "Mongolian t�gr�g",
+    "FIELD3": "MNT"
+  },
+  {
+    "FIELD1": "Montenegro",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Morocco",
+    "FIELD2": "Moroccan dirham",
+    "FIELD3": "MAD"
+  },
+  {
+    "FIELD1": "Mozambique",
+    "FIELD2": "Mozambican metical",
+    "FIELD3": "MZN"
+  },
+  {
+    "FIELD1": "Myanmar",
+    "FIELD2": "Burmese kyat",
+    "FIELD3": "MMK"
+  },
+  {
+    "FIELD1": "Namibia",
+    "FIELD2": "Namibian dollar",
+    "FIELD3": "NAD"
+  },
+  {
+    "FIELD1": "Nauru",
+    "FIELD2": "Australian dollar",
+    "FIELD3": "AUD"
+  },
+  {
+    "FIELD1": "Nepal",
+    "FIELD2": "Nepalese rupee",
+    "FIELD3": "NPR"
+  },
+  {
+    "FIELD1": "Netherlands",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "New Zealand",
+    "FIELD2": "New Zealand dollar",
+    "FIELD3": "NZD"
+  },
+  {
+    "FIELD1": "Nicaragua",
+    "FIELD2": "Nicaraguan c�rdoba",
+    "FIELD3": "NIO"
+  },
+  {
+    "FIELD1": "Niger",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Nigeria",
+    "FIELD2": "Nigerian naira",
+    "FIELD3": "NGN"
+  },
+  {
+    "FIELD1": "Norway",
+    "FIELD2": "Norwegian krone",
+    "FIELD3": "NOK"
+  },
+  {
+    "FIELD1": "Oman",
+    "FIELD2": "Omani rial",
+    "FIELD3": "OMR"
+  },
+  {
+    "FIELD1": "Pakistan",
+    "FIELD2": "Pakistani rupee",
+    "FIELD3": "PKR"
+  },
+  {
+    "FIELD1": "Palau",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Palestine",
+    "FIELD2": "Israeli new shekel",
+    "FIELD3": "ILS"
+  },
+  {
+    "FIELD1": "Panama",
+    "FIELD2": "Panamanian balboa",
+    "FIELD3": "PAB"
+  },
+  {
+    "FIELD1": "Papua New Guinea",
+    "FIELD2": "Papua New Guinean kina",
+    "FIELD3": "PGK"
+  },
+  {
+    "FIELD1": "Paraguay",
+    "FIELD2": "Paraguayan guaran�",
+    "FIELD3": "PYG"
+  },
+  {
+    "FIELD1": "Peru",
+    "FIELD2": "Peruvian sol",
+    "FIELD3": "PEN"
+  },
+  {
+    "FIELD1": "Philippines",
+    "FIELD2": "Philippine peso",
+    "FIELD3": "PHP"
+  },
+  {
+    "FIELD1": "Poland",
+    "FIELD2": "Polish zloty",
+    "FIELD3": "PLN"
+  },
+  {
+    "FIELD1": "Portugal",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Qatar",
+    "FIELD2": "Qatari riyal",
+    "FIELD3": "QAR"
+  },
+  {
+    "FIELD1": "Republic of the Congo",
+    "FIELD2": "Central African CFA franc",
+    "FIELD3": "XAF"
+  },
+  {
+    "FIELD1": "Romania",
+    "FIELD2": "Romanian leu",
+    "FIELD3": "RON"
+  },
+  {
+    "FIELD1": "Russia",
+    "FIELD2": "Russian ruble",
+    "FIELD3": "RUB"
+  },
+  {
+    "FIELD1": "Rwanda",
+    "FIELD2": "Rwandan franc",
+    "FIELD3": "RWF"
+  },
+  {
+    "FIELD1": "Saint Kitts and Nevis",
+    "FIELD2": "East Caribbean dollar",
+    "FIELD3": "XCD"
+  },
+  {
+    "FIELD1": "Saint Lucia",
+    "FIELD2": "East Caribbean dollar",
+    "FIELD3": "XCD"
+  },
+  {
+    "FIELD1": "Saint Vincent and the Grenadines",
+    "FIELD2": "East Caribbean dollar",
+    "FIELD3": "XCD"
+  },
+  {
+    "FIELD1": "Samoa",
+    "FIELD2": "Samoan tala",
+    "FIELD3": "WST"
+  },
+  {
+    "FIELD1": "San Marino",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Saudi Arabia",
+    "FIELD2": "Saudi riyal",
+    "FIELD3": "SAR"
+  },
+  {
+    "FIELD1": "Senegal",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Serbia",
+    "FIELD2": "Serbian dinar",
+    "FIELD3": "RSD"
+  },
+  {
+    "FIELD1": "Seychelles",
+    "FIELD2": "Seychellois rupee",
+    "FIELD3": "SCR"
+  },
+  {
+    "FIELD1": "Sierra Leone",
+    "FIELD2": "Sierra Leonean leone",
+    "FIELD3": "SLL"
+  },
+  {
+    "FIELD1": "Singapore",
+    "FIELD2": "Singapore dollar",
+    "FIELD3": "SGD"
+  },
+  {
+    "FIELD1": "Slovakia",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Slovenia",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Solomon Islands",
+    "FIELD2": "Solomon Islands dollar",
+    "FIELD3": "SBD"
+  },
+  {
+    "FIELD1": "Somalia",
+    "FIELD2": "Somali shilling",
+    "FIELD3": "SOS"
+  },
+  {
+    "FIELD1": "South Africa",
+    "FIELD2": "South African rand",
+    "FIELD3": "ZAR"
+  },
+  {
+    "FIELD1": "South Sudan",
+    "FIELD2": "South Sudanese pound",
+    "FIELD3": "SSP"
+  },
+  {
+    "FIELD1": "Spain",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Sri Lanka",
+    "FIELD2": "Sri Lankan rupee",
+    "FIELD3": "LKR"
+  },
+  {
+    "FIELD1": "Sudan",
+    "FIELD2": "Sudanese pound",
+    "FIELD3": "SDG"
+  },
+  {
+    "FIELD1": "Suriname",
+    "FIELD2": "Surinamese dollar",
+    "FIELD3": "SRD"
+  },
+  {
+    "FIELD1": "Swaziland",
+    "FIELD2": "Swazi lilangeni",
+    "FIELD3": "SZL"
+  },
+  {
+    "FIELD1": "Sweden",
+    "FIELD2": "Swedish krona",
+    "FIELD3": "SEK"
+  },
+  {
+    "FIELD1": "Switzerland",
+    "FIELD2": "Swiss franc",
+    "FIELD3": "CHF"
+  },
+  {
+    "FIELD1": "Syria",
+    "FIELD2": "Syrian pound",
+    "FIELD3": "SYP"
+  },
+  {
+    "FIELD1": "Taiwan",
+    "FIELD2": "New Taiwan dollar",
+    "FIELD3": "TWD"
+  },
+  {
+    "FIELD1": "Tajikistan",
+    "FIELD2": "Tajikistani somoni",
+    "FIELD3": "TJS"
+  },
+  {
+    "FIELD1": "Tanzania",
+    "FIELD2": "Tanzanian shilling",
+    "FIELD3": "TZS"
+  },
+  {
+    "FIELD1": "Thailand",
+    "FIELD2": "Thai baht",
+    "FIELD3": "THB"
+  },
+  {
+    "FIELD1": "Togo",
+    "FIELD2": "West African CFA franc",
+    "FIELD3": "XOF"
+  },
+  {
+    "FIELD1": "Tonga",
+    "FIELD2": "Tongan pa'anga",
+    "FIELD3": "TOP"
+  },
+  {
+    "FIELD1": "Trinidad and Tobago",
+    "FIELD2": "Trinidad and Tobago dollar",
+    "FIELD3": "TTD"
+  },
+  {
+    "FIELD1": "Tunisia",
+    "FIELD2": "Tunisian dinar",
+    "FIELD3": "TND"
+  },
+  {
+    "FIELD1": "Turkey",
+    "FIELD2": "Turkish lira",
+    "FIELD3": "TRY"
+  },
+  {
+    "FIELD1": "Turkmenistan",
+    "FIELD2": "Turkmenistan manat",
+    "FIELD3": "TMT"
+  },
+  {
+    "FIELD1": "Tuvalu",
+    "FIELD2": "Australian dollar",
+    "FIELD3": "AUD"
+  },
+  {
+    "FIELD1": "Uganda",
+    "FIELD2": "Ugandan shilling",
+    "FIELD3": "UGX"
+  },
+  {
+    "FIELD1": "Ukraine",
+    "FIELD2": "Ukrainian hryvnia",
+    "FIELD3": "UAH"
+  },
+  {
+    "FIELD1": "United Arab Emirates",
+    "FIELD2": "United Arab Emirates dirham",
+    "FIELD3": "AED"
+  },
+  {
+    "FIELD1": "United Kingdom",
+    "FIELD2": "British pound",
+    "FIELD3": "GBP"
+  },
+  {
+    "FIELD1": "United States",
+    "FIELD2": "United States dollar",
+    "FIELD3": "USD"
+  },
+  {
+    "FIELD1": "Uruguay",
+    "FIELD2": "Uruguayan peso",
+    "FIELD3": "UYU"
+  },
+  {
+    "FIELD1": "Uzbekistan",
+    "FIELD2": "Uzbekistani som",
+    "FIELD3": "UZS"
+  },
+  {
+    "FIELD1": "Vanuatu",
+    "FIELD2": "Vanuatu vatu",
+    "FIELD3": "VUV"
+  },
+  {
+    "FIELD1": "Vatican City",
+    "FIELD2": "Euro",
+    "FIELD3": "EUR"
+  },
+  {
+    "FIELD1": "Venezuela",
+    "FIELD2": "Venezuelan bolivar",
+    "FIELD3": "VEF"
+  },
+  {
+    "FIELD1": "Vietnam",
+    "FIELD2": "Vietnamese dong",
+    "FIELD3": "VND"
+  }
+ ]
+
+ arr = arr.map(ob => {return {
+  country: ob.FIELD1,
+  currency: ob.FIELD2,
+  code: ob.FIELD3,
+ }})
+
+
+const fs = require('fs');
+
+fs.writeFileSync('curr.json', JSON.stringify(arr, null, 2));
