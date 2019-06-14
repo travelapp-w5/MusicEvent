2bfccb1154a5e43d9c194f3233079a7765a6ffa7
stefkwan <stefanikwan@gmail.com>
2019-06-14 12:57:28 +0700

    add favorite

--- server/helpers/getToken.js
+++ server/helpers/getToken.js
@@ -5,7 +5,7 @@ module.exports = (input) => {
   let token = jwt.sign({
     id: input.id, 
     username: input.username
-  }, process.env.JWT_SECRET )
+  }, process.env.JWT_SECRET, {expiresIn: '4h'} )
 
   return token
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const ControllerUser = require('../controllers/users')
-
+// /api/users
 router.get('/', ControllerUser.findAll)
 router.post('/register', ControllerUser.create)
 router.post('/login', ControllerUser.login)
+
+const isAuthenticated = require('../middlewares/authentication')
+router.post('/addFav/:eventId', isAuthenticated, ControllerUser.addFav)
 
 
 module.exports = router
