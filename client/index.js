344320eabb329d8f901f8ed36c50a417fee48aaa
jaysm12 <jaizm12@gmail.com>
2019-06-14 10:03:54 +0700

    login-register

--- client/index.js
+++ client/index.js
@@ -26,3 +26,90 @@ $(document).ready(() => {
       .always(() => {})
   })
 });
+// <| ------- google sign-in -------|>
+
+function accesToken(token) {
+  localStorage.setItem('token', token)
+}
+
+function onSignIn(googleUser) {
+  const idToken = googleUser.AuthResponse().id_token
+
+  $.ajax({
+    method: "POST",
+    url:"api/users/googleSignin",
+    data: { idToken }
+  })
+  .done((result) => {
+    localStorage.setItem('token', result.token)
+    alert('Success signed in with google')
+  })
+  .fail((err) => {
+    alert(err.responseJSON.message)
+  })
+}
+
+function signOut() {
+  let auth2 = gapi.auth2.getAuthInstance();
+  auth2
+    .signOut()
+    .then(() => {
+      console.log('User signed out.')
+     });
+}
+
+// --------- sign in --------- 
+
+$('#login-form').submit(() => {
+  event.preventDefault()
+  let inputEmail = $('#loginEmail').val()
+  let inputPassword = $('#loginPassword').val()
+  $.ajax({
+    method: "POST",
+    url: "api/users/login",
+    data: {
+      email: inputEmail,
+      password: inputPassword
+    }
+  })
+  .done((result) => {
+    accesToken(result)
+    alert('Success signed in')
+  })
+  .fail(err => {
+    alert(err.responseJSON.message)
+  })
+})
+
+
+
+// -------- Register ------------
+
+$('#register-form').submit(() => {
+  event.preventDefault()
+  let inputEmail = $('#registrationEmail').val()
+  let inputPassword = $('#registrationPassword').val()
+
+  $.ajax({ 
+    method: "POST",
+    url: "api/users/register",
+    data: {
+      email: inputEmail,
+      password: inputPassword
+    }
+  })
+  .done((result) => {
+    alert('Succes created an account')
+  })
+  .fail(err => {
+    alert(err.responseJSON.message)
+  })
+})
+
+// ---------- Sign Out ---------------
+
+
+function logout() {
+  signOut()
+  localStorage.removeItem('token')
+}
--- server/helpers/getToken.js
+++ server/helpers/getToken.js
@@ -2,10 +2,7 @@ let jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 module.exports = (input) => {
-  let token = jwt.sign({
-    id: input.id, 
-    username: input.username
-  }, process.env.JWT_SECRET )
+  let token = jwt.sign( input , process.env.JWT_SECRET )
 
   return token
 }
