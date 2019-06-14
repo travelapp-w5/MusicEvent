9fee2aab18058c84dc4c0d9ce2e21b1c042ff97c
Max Tedjojuwono <maxpt750@gmail.com>
2019-06-14 13:07:19 +0700

    show list in client

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 const baseUrl = 'http://localhost:3000/';
+// Data from stef's endpoint
+let holidayArr
+
 // Format form inputs to json-like
 function format(form) {
   let out = {};
@@ -8,27 +11,101 @@ function format(form) {
   return out
 }
 
+function appendHoliday(obj) {
+  let datestring = new Date(obj.date).toDateString()
+  $('#list1').append(
+    `<div class="holiday">
+    <p>
+      ${datestring}<br>
+      ${obj.detail}
+    </p>
+    </div>`)
+}
+
+function appendEvent(obj) {
+  $('#list2').append(
+    `<div class="event">
+      <a href="${obj.uri}">${obj.displayName}</a><br>
+      <p>
+        Start date: ${obj.startDate}<br>
+        Venue: ${obj.venue}&nbsp;&nbsp;&nbsp;City: ${obj.city}<br>
+        Artists: ${obj.artists}<br>
+        Currency: ${obj.currency}&nbsp;&nbsp;&nbsp;Rate: ${obj.rate}<br>
+      </p>
+      <button class="button button1" onclick="addFav('${obj.eventId}')">Save</button>
+    </div>`)
+}
+
+function appendFavorite(obj) {
+  // Later
+}
+
+function addFav(id) {
+  // Jays isi ini
+  alert(id)
+}
+
+function appendCurrency(obj) {
+  $('#currencyinfo').html(
+    `<div>
+    <u>Currency Name</u>:<br>
+    ${obj.currency}</div>
+    <div><u>Rate</u>:<br>
+    1 ${obj.currency} = ${obj.rate} Rupiah
+    </div>`)
+}
+
 $(document).ready(() => {
-  $('#search-form').on('submit', function(event) {
+  // Search Form submit
+  $('#search1').on('submit', function(event) {
     event.preventDefault();
-    let input = format(this).search
-    // $('#clear').hide();
-    $('#search-status').html('Loading . . .');
+    let input = format(this)
     $.ajax({
-      url: `${baseUrl}search/`,
-      method: 'GET',
+      url: `${baseUrl}api/events/`,
+      method: 'POST',
       data: input,
     })
-      .done(data => {})
+      .done(data => {
+        console.log(data);
+        appendCurrency(data);
+        $('#list2').empty();
+        for(let event of data.events) {
+          appendEvent(event);
+        }
+      })
       .fail((jqXHR, textStatus) => {
         console.log('Failed: ', textStatus)
       })
-      .always(() => {})
+
+    $.ajax({
+      url: `${baseUrl}api/holidays/nextholidays`,
+      method: 'POST',
+      data: input,
+    })
+      .done(data => {
+        console.log(data)
+        holidayArr = data;
+        $('#list1').empty();
+        for(let holiday of data) {
+          appendHoliday(holiday)
+        }
+      })
+      .fail((jqXHR, textStatus) => {
+        console.log('Failed: ', textStatus)
+      })
+  })
+
+  $('#ok1').on('click', function() {
+    $('#reg-message').hide()
+    $('#front-forms').show()
+    $('.goog').show()
   })
 });
+
+
 // <| ------- google sign-in -------|>
 
-function accesToken(token) {
+function accessToken(token) {
   localStorage.setItem('token', token)
 }
 
@@ -42,6 +119,8 @@ function onSignIn(googleUser) {
   .done((result) => {
     localStorage.setItem('token', result.token)
     alert('Success signed in with google')
+    $('#outside').hide();
+    $('#inside').show();
   })
   .fail((err) => {
     // console.log(err.responseJSON.message)
@@ -59,7 +138,6 @@ function signOut() {
 }
 
 // --------- sign in --------- 
-
 $('#login-form').submit(() => {
   event.preventDefault()
   let inputEmail = $('#loginEmail').val()
@@ -73,18 +151,18 @@ $('#login-form').submit(() => {
     }
   })
   .done((result) => {
-    accesToken(result)
-    alert('Success signed in')
+    $('#loginEmail').val('')
+    $('#loginPassword').val('')
+    accessToken(result)
+    $('#outside').hide();
+    $('#inside').show();
   })
   .fail(err => {
     alert(err.responseJSON.message)
   })
 })
 
-
-
 // -------- Register ------------
-
 $('#register-form').submit((event) => {
   event.preventDefault()
   let inputEmail = $('#registrationEmail').val()
@@ -99,17 +177,11 @@ $('#register-form').submit((event) => {
     }
   })
   .done((result) => {
-    alert('Succes created an account')
+    $('#front-forms').hide()
+    $('.goog').hide()
+    $('#reg-message').show()
   })
   .fail(err => {
     alert(err.responseJSON.message)
   })
 })
-
-// ---------- Sign Out ---------------
-
-
-function logout() {
-  signOut()
-  localStorage.removeItem('token')
-}
