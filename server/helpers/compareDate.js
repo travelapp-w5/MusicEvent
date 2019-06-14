e4e907705a0a72bb90f67894e3ecabe6fe2b4200
stefkwan <stefanikwan@gmail.com>
2019-06-14 13:43:56 +0700

    helper holiday checker

--- server/helpers/compareDate.js
+++ server/helpers/compareDate.js
@@ -1,4 +1,4 @@
-module.exports = (date1, date2) => {
+function compareDate (date1, date2) {
 	date1 = new Date(date1)
 	date2 = new Date(date2)
 	date1.setHours(0, 0, 0, 0)
@@ -6,3 +6,15 @@ module.exports = (date1, date2) => {
 	
 	return date1.getTime() == date2.getTime()
 }
+
+function holidayOrNot(holArray, curDate){
+	let isHoliday = false;
+	holArray.forEach(hol => {
+		if(compareDate(hol.date, curDate)){
+			isHoliday = true
+		}
+	})
+	return isHoliday
+}
+
+module.exports = holidayOrNot
